d0b0567a8ccfc5649605dd9b4bb5394e9011f255
qzw0014 <qzw0014@tigermail.auburn.edu>
2016-10-08 20:18:53 -0500

    new branch update

---
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -0,0 +1,10 @@
+class MyClass(object):
+    
+    def __init__(self, logFile):
+        pass
+    
+    def setSightingFile(self, sightingFile):
+        pass
+    
+    def getSightings(self):
+        pass
---
+++ SoftwareProcess/Navigation/test/FixTest.py
@@ -0,0 +1,10 @@
+import unittest
+import Navigation.prod.Fix
+
+class FixTest(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
