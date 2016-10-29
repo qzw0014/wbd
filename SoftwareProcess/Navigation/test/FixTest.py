92c7fa1db0a71c768285006925d603610109d23f
qzw0014 <qzw0014@tigermail.auburn.edu>
2016-10-29 09:54:42 -0500

    Test case

--- SoftwareProcess/Navigation/test/FixTest.py
+++ SoftwareProcess/Navigation/test/FixTest.py
@@ -1,6 +1,6 @@
 import unittest
 import Navigation.prod.Fix as Fix
-
+import os
 class FixTest(unittest.TestCase):
     
     def setUp(self):
@@ -17,7 +17,9 @@ class FixTest(unittest.TestCase):
 #                An instance of Fix
 #            state change
 #                Writes the following entry to the log file:
-#                    Start of log
+#                    The literal string "Log file:"                   followed by
+#                    a tab (i.e., "\t")                               followed by
+#                    the absolute file path of the log file            
 #            Happy path
 #                nominal case with string input: Fix("log.txt")
 #                nominal case missing input: Fix()
@@ -48,19 +50,21 @@ class FixTest(unittest.TestCase):
 #            inputs
 #                name of XML file express as string which has a length .GE. 1
 #            outputs
-#                A string having the value passed as the "sightingFile".
+#                A string whose value is the absolute file path of the file specified by the parameter.              
 #            state change
 #                Writes the following entry to the log file:
-#                    Start of sighting file f.xml
-#                    where f.xml is the actual name of the file
+#                   The literal, "Sighting file:"                    followed by
+#                    a tab (i.e., "\t")                              followed by
+#                    the absolute file path of the sighting file            
 #            Happy path
 #                nominal case with string input: setSightingFile("sf.xml")
 #            Sad path
 #                The file name is invalid      
 #    Happy path
-    def test200_10_ShouldReturnString(self):
+    def test200_010_ShouldReturnStringFilePath(self):
         anFix = Fix.Fix()
-        self.assertIsInstance(anFix.setSightingFile("sf.xml"), str)
+        sigthingFilePath = os.path.abspath("sf.xml")
+        self.assertEqual(anFix.setSightingFile("sf.xml"), sigthingFilePath)
 #    Sad path
     def test200_910_ShouldRaiseIssueOnInvalidFileName(self):
         expectedDiag = self.className + "setSightingFile"
@@ -70,6 +74,54 @@ class FixTest(unittest.TestCase):
         self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)]) 
 
 #    Acceptance Test: 300
+#        Analysis - setAriesFile
+#            inputs
+#                name of Aries file express as string which has a length .GE. 1 and ".txt" is literal file extension
+#            outputs
+#                A string whose value is the absolute file path of the file specified by the parameter.              
+#            state change
+#                Writes the following entry to the log file:
+#                    The literal, "Aries file:"             followed by 
+#                            a tab (i.e., "\t")             followed by 
+#                            the absolute file path of the Aries file
+#            Happy path
+#                nominal case with string input: setAriesFile("aries.txt")
+#            Sad path
+#                The file name is invalid      
+#    Happy path
+    def test300_010_ShouldReturnStringFilePath(self):
+        aFix = Fix.Fix()
+        ariesFilePath = os.path.abspath("aries.txt")
+        self.assertEqual(aFix.setAriesFile("aries.txt"), ariesFilePath)
+#    Sad path
+    def test300_910_ShouldRaiseIssueOnInvalidFileName(self):
+        expectedDiag = self.className + "setAriesFile"
+        aFix =Fix.Fix()
+        with self.assertRaises(ValueError) as context:
+            aFix.setAriesFile()
+        self.assertEqual(expectedDiag, context.exception.args[0][0:len(expectedDiag)])
+#    Acceptance Test: 400
+#        Analysis - setStarFile
+#            inputs
+#                name of Aries file express as string which has a length .GE. 1 and ".txt" is literal file extension
+#            outputs
+#                A string whose value is the absolute file path of the file specified by the parameter.              
+#            state change
+#                Writes the following entry to the log file:
+#                    The literal, "Star file:"              followed by 
+#                            a tab (i.e., "\t")             followed by 
+#                            the absolute file path of the Star file
+#            Happy path
+#                nominal case with string input: setStarFile("stars.txt")
+#            Sad path
+#                The file name is invalid      
+#    Happy path
+    def test400_010_ShouldReturnStringFilePath(self):
+        aFix = Fix.Fix()
+        starsFilePath = os.path.abspath("stars.txt")
+        self.assertEqual(aFix.setStarFile("stars.txt"), starsFilePath)
+        
+#    Acceptance Test: 500
 #        Analysis - getSightings
 #            inputs
 #                none
@@ -84,40 +136,40 @@ class FixTest(unittest.TestCase):
 #                Errors are encountered in the sighting file
 #                The observed altitude is .LT. 0.1 arc-minutes
 #   Happy path
-    def test300_010_ShouldReturnLaAndLoOfLocation(self):
+    def test500_010_ShouldReturnLaAndLoOfLocation(self):
         anFix = Fix.Fix()
         assertResult = "0d0.0", "0d0.0"
         anFix.setSightingFile("sf.xml")
         self.assertEqual(assertResult, anFix.getSightings())
 #   Sad path
-    def test300_910_ShouldRaiseIssueOnNoSightingFileSet(self):
+    def test500_910_ShouldRaiseIssueOnNoSightingFileSet(self):
         expectedDiag = self.className + "getSightings"
         anFix = Fix.Fix()
         with self.assertRaises(ValueError) as context:
             anFix.getSightings()
         self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)])
-    def test300_920_ShouldRaiseIssueOnEncounterWithoutBodyInSightingFile(self):
+    def test500_920_ShouldRaiseIssueOnEncounterWithoutBodyInSightingFile(self):
         expectedDiag = self.className + "getSightings"
         anFix = Fix.Fix()
         anFix.setSightingFile("ErrorSightingsWithoutBody.xml")
         with self.assertRaises(ValueError) as context:
             anFix.getSightings()
         self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)])
-    def test300_930_ShouldRaiseIssueOnEncounterIncorrectDateInSightingFile(self):
+    def test500_930_ShouldRaiseIssueOnEncounterIncorrectDateInSightingFile(self):
         expectedDiag = self.className + "getSightings"
         anFix = Fix.Fix()
         anFix.setSightingFile("ErrorSightingsIncorrectDate.xml")
         with self.assertRaises(ValueError) as context:
             anFix.getSightings()
         self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)])
-    def test300_940_ShouldRaiseIssueOnEncounterInvalidHeightInSightingFile(self):
+    def test500_940_ShouldRaiseIssueOnEncounterInvalidHeightInSightingFile(self):
         expectedDiag = self.className + "getSightings"
         anFix = Fix.Fix()
         anFix.setSightingFile("ErrorSightingInvalidHeight.xml")
         with self.assertRaises(ValueError) as context:
             anFix.getSightings()
         self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)])
-    def test300_950_ShouldRaiseIssueOnObservationLessThanMinimumArcMinutes(self):
+    def test500_950_ShouldRaiseIssueOnObservationLessThanMinimumArcMinutes(self):
         expectedDiag = self.className + "getSightings"
         anFix = Fix.Fix()
         anFix.setSightingFile("ErrorSightingsInvalidObservation.xml")
